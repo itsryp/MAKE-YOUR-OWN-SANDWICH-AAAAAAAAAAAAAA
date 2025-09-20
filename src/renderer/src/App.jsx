f2581dc5ea1094b401db04a64667f902125b13e3
itsryp <droydumont@gmail.com>
2025-09-20 18:41:53 -0400

    smooth list

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -63,39 +63,66 @@ function App() {
       <div
         style={{
           display: 'flex',
-          height: '50vh'
+          height: '50vh',
+          width: '100vw',
+          flexDirection: 'column'
         }}>
-        {
           <div
             style={{
-              width: '90vw',
-              display: 'flex',
-              overflow: 'hidden',
-              justifyContent: 'center'
+              width: '100%',
+              overflow: 'hidden'
             }}>
-            <img
-              src={toppings[currentTopping - 1 < 0 ? toppings.length - 1 : currentTopping - 1]}
-
-              onMouseDown={handlePrev}
-              className="adjacentTopping"
-            ></img>
-            <img
-              src={toppings[currentTopping]}
-              style={{
-                height: '40vh',
-                zIndex: 2,
-                width: '50%',
-                objectFit: 'contain'
-              }}
-              onClick={handleAdd}
-            ></img>
-            <img
-              src={toppings[(currentTopping + 1) % toppings.length]}
-              onMouseDown={handleNext}
-              className="adjacentTopping"
-            ></img>
+              {toppings.map((topping, index) => {
+                const distance = Math.abs(currentTopping-index);
+                return (
+                  <img
+                    src={topping}
+                    style={{
+                      width: '50vw',
+                      height: '50vh',
+                      objectFit: 'contain',
+                      position: 'fixed',
+                      left: '50vw',
+                      translate: `${((index - currentTopping)*50)-50}%`,
+                      zIndex: `${toppings.length-Math.abs(currentTopping-index)}`,
+                      opacity: 1-distance*.5,
+                      transition: 'all 0.5s ease',
+                      // rotate: `y ${distance*45}deg`,
+                      scale: 1-distance*.3,
+                      filter: 'drop-shadow(0 12px 24px #000000)'
+                    }}
+                    // onMouseDown={index != currentTopping ? (index<currentTopping?handlePrev:handleNext) : null}
+                ></img>
+                )
+              })}
           </div>
-        }
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                height: '10vh',
+                position: 'fixed',
+                bottom: 0,
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                zIndex: 1000
+              }}>
+                <button
+                  className='control-button'
+                  onMouseDown={handlePrev}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20z"/></svg>
+                </button>
+                <button
+                  className='control-button'
+                  onMouseDown={handleAdd}>
+                  <svg style={{scale: 1.5}} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"/></svg>
+                  </button>
+                <button
+                  className='control-button'
+                  onMouseDown={handleNext}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
+                  </button>
+            </div>
       </div>
     </>
   )
